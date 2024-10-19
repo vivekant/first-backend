2e01575fa1008e019ee24bb498eac57356508b7e
mahakal <mahakal602@gmail.com>
2024-10-19 23:05:57 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import 'dotenv/config'
+const app=express()
+// const port=3000
+
+app.get('/',(req,res) => {
+  res.send("First backend to deploy!")
+}
+)
+
+app.get('/:name',(req,res) => {
+  res.send(`Hellow ${req.params.name}`)
+}
+)
+
+
+app.listen(process.env.port,() => {
+  console.log(`app is listing on http://localhost:${process.env.port}/`);
+  
+}
+)
