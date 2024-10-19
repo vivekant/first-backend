ef5b05d918fe8640da2c30a205600927c80c2bdc
mahakal <mahakal602@gmail.com>
2024-10-19 23:23:31 +0530

    adding jsondata api

--- main.js
+++ main.js
@@ -7,11 +7,15 @@ app.get('/',(req,res) => {
   res.send("First backend to deploy!")
 }
 )
-
+app.get('/jsondata',(req,res) => {
+    res.json({name:"vivekant kumar",favguy:"dhara"})
+  }
+  )
 app.get('/:name',(req,res) => {
   res.send(`Hellow ${req.params.name}`)
 }
 )
+
 
 
 app.listen(process.env.port,() => {
